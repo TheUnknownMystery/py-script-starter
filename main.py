a871b31d048382bb610246810d5a27411eef1b48
Sauhardo Sengupta <sauhardo2007@gmail.com>
2022-07-01 20:23:18 +0530

    pyscript starter init commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import datetime as dt
+
+print('Welcome to pyscript')
+print('This is a simple python script')
+print('today', dt.date.today().strftime('%A %B %d, %Y'))
+for i in range(0, 100):
+    print(i)
